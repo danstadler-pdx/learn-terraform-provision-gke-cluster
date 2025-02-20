5df4b561f15750e85e3b49085f61dc4ae8ab0fad
Dan Stadler <dan.stadler@grafana.com>
2025-02-20 09:52:27 -0800

    bring in changes for custom demo cluster

--- gke.tf
+++ gke.tf
@@ -1,35 +1,39 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-variable "gke_username" {
-  default     = ""
-  description = "gke username"
-}
-
-variable "gke_password" {
-  default     = ""
-  description = "gke password"
-}
-
-variable "gke_num_nodes" {
-  default     = 2
-  description = "number of gke nodes"
-}
 
 # GKE cluster
 data "google_container_engine_versions" "gke_version" {
   location = var.region
-  version_prefix = "1.27."
+  version_prefix = "1.30."
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "${var.project_id}-gke"
+  name     = "my-takehome-cluster"
   location = var.region
+  deletion_protection = false
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = true
+  node_locations = [
+    "us-central1-a",
+  ]
+
+  monitoring_config {
+	enable_components = []     ## see discussion here: https://github.com/hashicorp/terraform-provider-google/issues/15056
+	managed_prometheus {
+		enabled = false
+	}
+	advanced_datapath_observability_config {
+		enable_metrics = false
+		enable_relay = false
+	}
+  }
+
+  logging_config {
+	enable_components = []
+  }
+
+  # to save time during TF runs, I leave this false, then later resize the default pool to 0 via the UI
+  remove_default_node_pool = false
   initial_node_count       = 1
 
   network    = google_compute_network.vpc.name
@@ -38,17 +42,26 @@ resource "google_container_cluster" "primary" {
 
 # Separately Managed Node Pool
 resource "google_container_node_pool" "primary_nodes" {
-  name       = google_container_cluster.primary.name
+  name       = "primary-nodes"
   location   = var.region
   cluster    = google_container_cluster.primary.name
-  
   version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
-  node_count = var.gke_num_nodes
+
+  node_count = 4
+  autoscaling {
+	min_node_count = "4"
+	max_node_count = "4"
+  }
+  # max_pods_per_node = "0"
 
   node_config {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+	  "https://www.googleapis.com/auth/devstorage.read_only",
+	  "https://www.googleapis.com/auth/service.management.readonly",
+	  "https://www.googleapis.com/auth/servicecontrol",
+	  "https://www.googleapis.com/auth/trace.append"
     ]
 
     labels = {
@@ -57,29 +70,44 @@ resource "google_container_node_pool" "primary_nodes" {
 
     # preemptible  = true
     machine_type = "n1-standard-1"
-    tags         = ["gke-node", "${var.project_id}-gke"]
+    tags         = ["node-group", "primary-nodes"]
     metadata = {
       disable-legacy-endpoints = "true"
     }
   }
 }
 
+resource "google_container_node_pool" "big_nodes" {
+  name       = "big-nodes"
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
 
-# # Kubernetes provider
-# # The Terraform Kubernetes Provider configuration below is used as a learning reference only. 
-# # It references the variables and resources provisioned in this file. 
-# # We recommend you put this in another file -- so you can have a more modular configuration.
-# # https://learn.hashicorp.com/terraform/kubernetes/provision-gke-cluster#optional-configure-terraform-kubernetes-provider
-# # To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/tutorials/terraform/kubernetes-provider.
+  node_count = 1
+  autoscaling {
+	min_node_count = "1"
+	max_node_count = "1"
+  }
 
-# provider "kubernetes" {
-#   load_config_file = "false"
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+	  "https://www.googleapis.com/auth/devstorage.read_only",
+	  "https://www.googleapis.com/auth/service.management.readonly",
+	  "https://www.googleapis.com/auth/servicecontrol",
+	  "https://www.googleapis.com/auth/trace.append"
+    ]
 
-#   host     = google_container_cluster.primary.endpoint
-#   username = var.gke_username
-#   password = var.gke_password
+    labels = {
+      env = var.project_id
+    }
 
-#   client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-#   client_key             = google_container_cluster.primary.master_auth.0.client_key
-#   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-# }
+    # preemptible  = true
+    machine_type = "n1-standard-4"
+    tags         = ["node-group", "big-nodes"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -16,13 +16,13 @@ provider "google" {
 
 # VPC
 resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
+  name                    = "my-takehome-vpc"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
+  name          = "my-takehome-subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
